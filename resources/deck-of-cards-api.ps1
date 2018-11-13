d13f6d4a373e1f9ad292736ebf5577556f772bf7
Abhinav Sharma <abhi18av@gmail.com>
2018-11-13 23:08:04 +0530

    added an example of the api request via pwsh

--- resources/deck-of-cards-api.ps1
+++ resources/deck-of-cards-api.ps1
@@ -1,1 +1,4 @@
+https://www.twilio.com/docs/usage/tutorials/how-to-make-http-basic-request-twilio-powershell
+
+
 Invoke-RestMethod -Method Get https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1 
