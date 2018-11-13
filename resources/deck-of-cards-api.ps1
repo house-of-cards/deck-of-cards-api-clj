68646e0eee3b631e2b60695facd207c8c1ad9bdf
Abhinav Sharma <abhi18av@gmail.com>
2018-11-13 23:22:03 +0530

    need to print out the entire response to the terminal via invoke-restmethod

--- resources/deck-of-cards-api.ps1
+++ resources/deck-of-cards-api.ps1
@@ -1,4 +1,13 @@
-https://www.twilio.com/docs/usage/tutorials/how-to-make-http-basic-request-twilio-powershell
+#https://www.twilio.com/docs/usage/tutorials/how-to-make-http-basic-request-twilio-powershell
 
+##########
 
 Invoke-RestMethod -Method Get https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1 
+
+#remaining shuffled success deck_id
+#--------- -------- ------- -------
+#       52     True    True 9n5ye8h2putj
+
+##########
+
+$var =  Invoke-RestMethod -Method Get https://deckofcardsapi.com/api/deck/9n5ye8h2putj/draw/?count=2
